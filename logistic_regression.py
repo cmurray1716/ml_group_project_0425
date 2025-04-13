0c51993d1af21ce3e068c43f425afc164855bd70
cmurray1716 <163459843+cmurray1716@users.noreply.github.com>
2025-04-13 20:11:09 +0100

    Update logistic_regression.py
    Added hyperparameter tuning

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,15 +1,18 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn import metrics
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import FeatureHasher
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.utils import shuffle
+from skopt import BayesSearchCV
+from imblearn.under_sampling import RandomUnderSampler, ClusterCentroids, TomekLinks, CondensedNearestNeighbour
+from imblearn.pipeline import Pipeline
 
 #reading in the data
 df = pd.read_csv("C:/Users/Colm/Downloads/genre_full.csv")
@@ -26,14 +29,50 @@ testy = val_df['Genre'].values
 #Using the logistic regression function in scikit
 log_model = LogisticRegression(max_iter=10000)
 #Using the TFID vectorizer to convert the lyrics to numbers
-vectorizer =  TfidfVectorizer(max_features=500)
+vectorizer =  TfidfVectorizer(max_features=100)
 trainx_v = vectorizer.fit_transform(trainx)
 testx_v = vectorizer.fit_transform(testx)
 
+#Huge class imbalance so adding undersampling
+rus = RandomUnderSampler(random_state=54)
+cluster = ClusterCentroids(random_state=54)
+cnn = CondensedNearestNeighbour(random_state=54)
+tl = TomekLinks()
+
+trainx_v_resampled, trainy_resampled = rus.fit_resample(trainx_v, trainy)
+#trainx_v_resampled, trainy_resampled = cnn.fit_resample(trainx_v, trainy)
+#trainx_v_resampled, trainy_resampled = tl.fit_resample(trainx_v, trainy)
+#trainx_v_resampled, trainy_resampled = cluster.fit_resample(trainx_v, trainy)
+
 #Running the model
-log_model.fit(trainx_v, trainy)
+log_model.fit(trainx_v_resampled, trainy_resampled)
 #Testing the model
 predictions = log_model.predict(testx_v)
 
+print("Confusion Matrix:")
 print(confusion_matrix(testy, predictions))
+print("Accuracy Score:")
 print(accuracy_score(testy, predictions))
+print("Precision Score:")
+print(precision_score(testy, predictions, average = "weighted"))
+print("Recall Score:")
+print(recall_score(testy, predictions, average = "weighted"))
+print("F1 Score:")
+print(f1_score(testy, predictions, average = "weighted"))
+
+pipeline = Pipeline([('tfidf', TfidfVectorizer()), ("ran", RandomUnderSampler()), ('lgr', LogisticRegression(max_iter=10000))])
+
+#Setting up parameters for hyperparameter tuning
+solvers = ["newton-cg", "lbfgs", "sag", "saga"]
+penalty = ['l2']
+c_values = np.linspace(0.01, 100, num=20)
+min_df = np.linspace(0.01, 0.5, num=20)
+max_features = np.arange(1,1100)
+
+grid = dict(lgr__solver = solvers, lgr__penalty = penalty, lgr__C = c_values, tfidf__min_df = min_df, tfidf__max_features = max_features)
+
+random_search = RandomizedSearchCV(estimator = pipeline, param_distributions=grid, n_iter=3, cv=3, random_state=42)
+
+random_search.fit(trainx, trainy)
+print("Best Parameters:", random_search.best_params_)
+print("Best Score:", random_search.best_score_)
